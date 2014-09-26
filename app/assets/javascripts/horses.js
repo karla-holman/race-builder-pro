4f424888398dfb3c8ca2827a927927715020ec1c
Owen Hope <owen@hopemediahouse.com>
2014-09-25 19:02:08 -0700

    Updating gems

--- app/assets/javascripts/horses.js
+++ app/assets/javascripts/horses.js
@@ -1,44 +1,44 @@
 $(document).ready(function() {
 	//Creating table with specific colors depending on the horse status
-	$('#stable-table').dataTable({
-    sPaginationType: "full_numbers",
-    "fnRowCallback": function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
-      switch(aData[6]){
-        case 'Race Ready':
-          $('td', nRow).eq(6).css('color', 'green')
-          break;
-        case 'Not Race Ready':
-          $('td', nRow).eq(6).css('color', 'red')
-          break;
-        case 'Inactive':
-          $('td', nRow).eq(6).css('color', 'red')
-          break;
-        case 'Resting From Race':
-          $('td', nRow).eq(6).css('color', 'yellow')
-          break;
-        case 'Steward\'s List':
-          $('td', nRow).eq(6).css('color', 'blue')
-          break;
-        case 'Vet\'s List':
-          $('td', nRow).eq(6).css('color', 'blue')
-          break;
-      }
-    }
-  });
+	// $('#stable-table').dataTable({
+ //    sPaginationType: "full_numbers",
+ //    "fnRowCallback": function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
+ //      switch(aData[6]){
+ //        case 'Race Ready':
+ //          $('td', nRow).eq(6).css('color', 'green')
+ //          break;
+ //        case 'Not Race Ready':
+ //          $('td', nRow).eq(6).css('color', 'red')
+ //          break;
+ //        case 'Inactive':
+ //          $('td', nRow).eq(6).css('color', 'red')
+ //          break;
+ //        case 'Resting From Race':
+ //          $('td', nRow).eq(6).css('color', 'yellow')
+ //          break;
+ //        case 'Steward\'s List':
+ //          $('td', nRow).eq(6).css('color', 'blue')
+ //          break;
+ //        case 'Vet\'s List':
+ //          $('td', nRow).eq(6).css('color', 'blue')
+ //          break;
+ //      }
+ //    }
+ //  });
 
-  $('#racestatus-table').dataTable({
-    sPaginationType: "full_numbers",
-    searching: false,
-    info: false
-		"fnDrawCallback": function( oSettings ) {
-			$(".edit_horserace").change(function(){
-				form = $(this).closest("form");
-				attemptUpdate(form);
-				location.reload()
-				return false;
-			});
-		}
-	});
+ //  $('#racestatus-table').dataTable({
+ //    sPaginationType: "full_numbers",
+ //    searching: false,
+ //    info: false
+	// 	"fnDrawCallback": function( oSettings ) {
+	// 		$(".edit_horserace").change(function(){
+	// 			form = $(this).closest("form");
+	// 			attemptUpdate(form);
+	// 			location.reload()
+	// 			return false;
+	// 		});
+	// 	}
+	// });
 
 	//When changing horse status, submit the form
 	$(".attribute").change(function(){
