73bc57958f895244288bfc81f8e40f65e3b14ed1
Owen Hope <owen@hopemediahouse.com>
2014-09-25 18:50:24 -0700

    Updating gemfile and removing files

--- Gemfile
+++ Gemfile
@@ -1,13 +1,10 @@
 source 'https://rubygems.org'
-ruby '2.1.1'
+#ruby '2.1.1'
 gem 'rails', '4.1.0.rc1'
 gem 'pg'
 gem 'sass-rails', '~> 4.0.1'
-gem 'bootstrap-sass'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
-gem "jquery-rails", "~> 2.3.0"
-gem 'jquery-ui-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
@@ -15,9 +12,7 @@ gem 'devise'
 gem 'pundit'
 gem "tzinfo-data"
 gem "simple_form"
-gem 'bootstrap-datetimepicker-rails'
 gem 'public_activity'
-gem 'jquery-datatables-rails'
 group :development do
   gem 'better_errors'
   gem 'binding_of_caller', :platforms=>[:mri_19, :mri_20, :mri_21, :rbx]
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,29 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require jquery.ui.all
-//= require bootstrap
-//= require bootstrap-datetimepicker
-//= require dataTables/jquery.dataTables
 //= require_tree .
 
 $(document).ready(function() {
-  //Date Picker Format
-  $(".date-picker").datepicker({
-  	dateFormat: "yy-mm-dd"
-	});
-
-  //Date Time Picker Format
-	$('#datetimepicker').datetimepicker({
-      format: 'yyyy-MM-dd hh:mm:ss'
-   });
-
-  //Tooltip options
-	$('[data-toggle="tooltip"]').tooltip({'placement': 'right'});
-
-  //Base Style for generic tables
-	$('.generic-table').dataTable({sPaginationType: "full_numbers"});
-
 })
