5f7143cb6b46a78c47f712ad1e3271cd786aca42
Kevin Walter <kevin@hopemediahouse.com>
2015-04-20 22:00:30 -0700

    attempt to log out user on browser close

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
+         :recoverable, :trackable, :validatable, :timeoutable
   enum role: [:user, :trainer, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
